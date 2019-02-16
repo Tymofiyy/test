59e67df7912e601796f24fe966b2d86a5e7fc037
illya <illuhawo@gmail.com>
2019-02-16 17:56:37 +0200

    commit

---
+++ js/jsIndex.js
@@ -0,0 +1,10 @@
+var uu="qweertyio";
+var u = "Lorem ipsum do34lor sit amet, consectet5667ur adipi7sicing elit. Voluptate voluptatum quam harum tempora quidem consequatur architecto fugit praesentium commodi repellendus quisquam perferendis, vel quis! Provident unde laborum consequatur aut quos."
+alert(u.slice("q", "r"));
+ for(var i=1; i<=u.length; i++){
+ 	alert(u.length-1);
+
+ }
+
+
+
